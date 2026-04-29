456423e51bd7dba07e885fc79f4de34d3ca73aec
Nico Dann <nicodann@gmail.com>
2026-04-29 14:16:52 -0400

    Initial commit.

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you!")
+	})
+
+	http.HandleFunc("/ai-query", )
+
+	fmt.Println("Server starting on :8080...")
+
+	http.ListenAndServe(":8080", nil)
+}
